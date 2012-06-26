828b1bfbfea0d27306fc99ac729262441488d95e
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2012-06-26 18:00:17 +0200

    Add comments.

--- fr.opensagres.xdocreport.remoting.resources/src/main/java/fr/opensagres/xdocreport/remoting/resources/domain/ResourceHelper.java
+++ fr.opensagres.xdocreport.remoting.resources/src/main/java/fr/opensagres/xdocreport/remoting/resources/domain/ResourceHelper.java
@@ -2,8 +2,19 @@ package fr.opensagres.xdocreport.remoting.resources.domain;
 
 import java.util.List;
 
+/**
+ * Resource Helper.
+ * 
+ */
 public class ResourceHelper {
 
+	/**
+	 * Find fields metadata file from the template resource (ex:
+	 * MyTemplate/META-INF/MyTemplate.fields.xml).
+	 * 
+	 * @param template
+	 * @return
+	 */
 	public static Resource findFieldsMetadataFromTemplate(Resource template) {
 		Resource metaInf = findMetaInfFromTemplate(template);
 		if (metaInf == null) {
@@ -12,6 +23,13 @@ public class ResourceHelper {
 		return findFieldsMetadataFromMetaInf(metaInf);
 	}
 
+	/**
+	 * Find META-INF category from the template resource (ex:
+	 * MyTemplate/META-INF).
+	 * 
+	 * @param template
+	 * @return
+	 */
 	public static Resource findMetaInfFromTemplate(Resource template) {
 		List<Resource> resources = template.getChildren();
 		for (Resource resource : resources) {
@@ -22,6 +40,13 @@ public class ResourceHelper {
 		return null;
 	}
 
+	/**
+	 * Find fields metadata file from the META-INF resource (ex:
+	 * META-INF/MyTemplate.fields.xml).
+	 * 
+	 * @param template
+	 * @return
+	 */
 	public static Resource findFieldsMetadataFromMetaInf(Resource metaInf) {
 		List<Resource> resources = metaInf.getChildren();
 		String name = null;
